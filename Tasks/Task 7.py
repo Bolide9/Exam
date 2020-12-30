9a38d2eeba9a60cb239ee10fa6ad7602836407e9
Bolide9 <dgabrailov2000@mail.ru>
2020-12-30 20:46:29 +0300

    Task 7

---
+++ Tasks/Task 7.py
@@ -0,0 +1,16 @@
+# Напишите программу калькулятор, которые принимает 2 числа и выполняет следующие операции: +, -, *, /.
+
+a = int(input())
+b = int(input())
+
+symb = str(input('Введите символ: '))
+
+
+if symb == '+':
+    print(a + b)
+if symb == '-':
+    print(a - b)
+if symb == '*':
+    print(a * b)
+if symb == '/':
+    print(a / b)
